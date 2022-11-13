97926e04d498e8f2c31c9f7ceb3b179fd20de8fa
Guilherme <guilherme.martin@MacBook-Pro-de-Guilherme.local>
2022-11-13 11:15:30 -0300

    Configuração API Gateway

---
+++ APIGateway/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseOcelot().Wait();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+
